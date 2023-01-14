0540b9b5037e7f9cc6a37e64c0bc7790673e47ed
walerick <bakareadewale24@gmail.com>
2023-01-14 14:16:08 +0100

    first commit

---
+++ src/Home.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+const { Configuration, OpenAIApi } = require("openai");
+
+const Home = () => {
+  const [textInput, setTextInput] = useState("");
+  const [result, setResult] = useState();
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    const configuration = new Configuration({
+      apiKey: "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",
+    });
+    const openai = new OpenAIApi(configuration);
+    try {
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: ` ${textInput}, summarize this to a 3 years old `,
+        temperature: 0.9,
+        max_tokens: 100,
+      });
+      setResult(response.data.choices[0].text);
+    } catch (err) {
+      console.log(err);
+      setResult("Error generating response");
+    }
+  }
+
+  return (
+    <main className="home">
+      <h3>Simplify words</h3>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="texts"
+          placeholder="Text here..."
+          value={textInput}
+          onChange={(e) => setTextInput(e.target.value)}
+        />
+        <input type="submit" value="Simplify" />
+      </form>
+
+      <div className="result">{result}</div>
+    </main>
+  );
+};
+
+export default Home;
